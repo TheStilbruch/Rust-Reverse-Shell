f4d793e74d55ea97f0cf282c39126adfef12b543
Hayden Pope <stilbruch39@gmail.com>
2018-02-15 20:01:11 +0000

    Added a command line arg that allows the user to specify port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 
 use std::net::{TcpListener, TcpStream};
 use std::thread;
+use std::env;
 use std::io::{Write, Read};
 use std::process::{Command, Stdio};
 use std::os::unix::io::{AsRawFd, FromRawFd};
@@ -17,6 +18,28 @@ unsafe fn handle_client(mut stream: TcpStream) {
 }
 
 fn main() {
+
+    let args: Vec<String> = env::args().collect();
+    let mut port: i32 = 25566;
+
+    match args.len() {
+        1 => {} //No args provided, skip
+        2 => {
+            match args[1].to_string().parse::<i32>() {
+                Ok(n) => port = n,
+                Err(_) => {
+                    eprintln!("Error: Invalid number!");
+                    return;
+                }
+            };
+        }
+        _ => eprintln!("Error: Too many args!"),
+    };
+
+    if port > 65535 || port < 1 {
+        eprintln!("Error: Port must be between 1 and 65535");
+        return;
+    }
 
     let listener = TcpListener::bind("127.0.0.1:25566").unwrap();
 
